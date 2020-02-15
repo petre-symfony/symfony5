1e10c68bcf8ba02cc26d19b649c6d22561ee37f1
petrero <petrero@gmail.com>
2020-02-15 12:29:33 +0200

    26.4.Validation Auto-Mapping{Auto-Mapping is Smart}
    Watch this: add a ? before string to make null an allowed value.
    Refresh now and... the error is gone.

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -103,7 +103,7 @@ class Article {
 		return $this->title;
 	}
 
-	public function setTitle(string $title): self {
+	public function setTitle(?string $title): self {
 		$this->title = $title;
 
 		return $this;
